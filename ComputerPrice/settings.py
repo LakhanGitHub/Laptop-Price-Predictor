a38890e29a61384380f70d389280520bb24a9bac
LakhanGitHub <109935532+LakhanGitHub@users.noreply.github.com>
2025-02-17 16:09:41 +0530

    Update settings.py

--- ComputerPrice/settings.py
+++ ComputerPrice/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-*z2&muenoknodf)eo^lsx&c=lb41j)l9$qc%a3e)emdyuqt$oe
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
